d90d00c86afe6a0747279ae4322b6ed59da03a47
Genne Liu <genneliu@gmail.com>
2021-11-02 09:31:24 -0700

    set timeout

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,11 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
+const theOneFunc = (delay) => {};
+    console.log("Hello " + delay + " seconds")
 
 // Hello after 4 seconds
+setTimeout(theOneFunc, 4 * 1000, 4)
+
 
 // Hello after 8 seconds
+setTimeout(theOneFunc, 8 * 1000, 8)
 
 // You can define only ONE function
